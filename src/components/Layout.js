09f335a4370ebe48a845cd1ecf35725ad6ae4534
Burhan SAVCI <burhansavci@gmail.com>
2020-12-25 00:07:09 +0300

    Adjust layout skeleton

---
+++ src/components/Layout.js
@@ -0,0 +1,109 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql, StaticQuery, Link } from "gatsby"
+import Img from "gatsby-image"
+
+// Styles
+import "../styles/blog.css"
+
+const Layout = ({ data, children, isHome }) => {
+  const { siteMetadata } = data.site
+  console.log(siteMetadata)
+  console.log(data)
+  return (
+    <>
+      <div className="viewport">
+
+        <div className="viewport-top">
+          <header className="site-head"
+                  style={{ ...(data.coverImage.childImageSharp.fluid.src && { backgroundImage: `url(${data.coverImage.childImageSharp.fluid.src})` }) }}>
+            <div className="container">
+
+              <div className="site-mast">
+
+                <div className="site-mast-left">
+                  <Link to="/">
+                    <Img fixed={data.logo.childImageSharp.fixed} alt={siteMetadata.title} />
+                  </Link>
+                </div>
+
+                <div className="site-mast-right">
+                  {/*Social links goes here*/}
+                </div>
+
+              </div>
+
+              {isHome ? (
+                <div className="site-banner">
+                  <h1 className="site-banner-title">{siteMetadata.title}</h1>
+                  <p className="site-banner-desc">{siteMetadata.description}</p>
+                </div>
+              ) : null}
+
+              <nav className="site-nav">
+                <div className="site-nav-left">
+                  {/*Nav component goes here*/}
+                </div>
+                <div className="site-nav-right">
+                  <Link className="site-nav-button" to="/about">
+                    About
+                  </Link>
+                </div>
+              </nav>
+
+            </div>
+          </header>
+
+          <main className="site-main">{children}</main>
+        </div>
+
+        <div className="viewport-bottom">
+          <footer className="site-foot">
+            <div className="site-foot-nav container">
+              <div className="site-foot-nav-right">
+                {/*Nav component goes here*/}
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired
+}
+
+const LayoutSettingsQuery = props => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            author
+            description
+          }
+        }
+        logo: file(relativePath: { eq: "icon.png" }) {
+          childImageSharp {
+            fixed(width: 30, height: 30) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+        coverImage: file(relativePath: { eq: "cover_image.jpg" }) {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => <Layout data={data} {...props} />}
+  />
+)
+
+export default LayoutSettingsQuery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
+  <Layout isHome={true}>
     <SEO title="Home" />
     <h1>Home Page</h1>
   </Layout>

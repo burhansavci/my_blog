d77143257f655ab3d7dee11e90d1a7b6b2ed999d
Burhan SAVCI <burhansavci@gmail.com>
2020-12-27 18:38:06 +0300

    Add missing field to graphql query

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -55,6 +55,7 @@ export const pageQuery = graphql`
                         tag
                     }
                     fields {
+                        slug
                         slugTag
                     }
                     excerpt

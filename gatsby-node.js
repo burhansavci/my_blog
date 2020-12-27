0404cea0c74e1616058242937a4e00c2e706a6b1
Burhan SAVCI <burhansavci@gmail.com>
2020-12-27 13:07:25 +0300

    Configure pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,60 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const { slugify } = require("./src/util/utilityFunctions")
+const path = require(`path`)
+const { paginate } = require(`gatsby-awesome-pagination`)
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  if (node.internal.type === "MarkdownRemark") {
+    const { createNodeField } = actions
+    const slugFromTitle = slugify(node.frontmatter.title)
+    createNodeField({
+      node,
+      name: "slug",
+      value: slugFromTitle
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+
+  const result = await graphql(`
+  {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`)
+
+
+
+
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const posts = result.data.allMarkdownRemark.edges
+
+  const indexTemplate = path.resolve(`./src/templates/index.js`)
+
+  // Create pagination
+  paginate({
+    createPage,
+    items: posts,
+    itemsPerPage: 6,
+    component: indexTemplate,
+    pathPrefix: ({ pageNumber }) => {
+      if (pageNumber === 0) {
+        return `/`
+      } else {
+        return `/page`
+      }
+    },
+  })
+
+
+}

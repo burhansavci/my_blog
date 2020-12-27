fd5b48b36c4e1a7491a4a03d7f5af1c93fabc3de
Burhan SAVCI <burhansavci@gmail.com>
2020-12-27 16:15:16 +0300

    Add disqus-react package

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,9 +4,18 @@ import { graphql, Link } from "gatsby"
 
 import Layout from "../components/Layout"
 import { slugify } from "../util/utilityFunctions"
+import { DiscussionEmbed } from "disqus-react"
 
-const Post = ({ data }) => {
+const Post = ({ data, pageContext }) => {
   const post = data.markdownRemark
+  const baseUrl = "https://burhansavci.netlify.app/"
+
+  const disqusShortname = "https-burhansavci-netlify-app"
+  const disqusConfig = {
+    identifier: data.markdownRemark.id,
+    title: post.title,
+    url: baseUrl + pageContext.slug
+  }
 
   return (
     <>
@@ -24,26 +33,27 @@ const Post = ({ data }) => {
                 <h1 className="post-full-title">{post.frontmatter.title}</h1>
 
                 <div className="post-full-byline">
-                    <section className="post-full-byline-content">
-                        <time dateTime={post.frontmatter.date}>{post.frontmatter.date}</time>
-                        <span className="bull">•</span>
-                        {post.timeToRead} min read
-                    </section>
+                  <section className="post-full-byline-content">
+                    <time dateTime={post.frontmatter.date}>{post.frontmatter.date}</time>
+                    <span className="bull">•</span>
+                    {post.timeToRead} min read
+                  </section>
                 </div>
 
               </header>
-
 
               <section
                 className="content-body load-external-scripts"
                 dangerouslySetInnerHTML={{ __html: post.html }}
               />
             </section>
+            <DiscussionEmbed style={{ margin: "0 auto", maxWidth: "840px" }} shortname={disqusShortname}
+                             config={disqusConfig} />
           </article>
         </div>
       </Layout>
     </>
-)
+  )
 }
 
 Post.propTypes =
@@ -51,7 +61,7 @@ Post.propTypes =
     data: PropTypes.shape({
       markdownRemark: PropTypes.shape({
         html: PropTypes.string.isRequired,
-        timeToRead:PropTypes.number.isRequired,
+        timeToRead: PropTypes.number.isRequired,
         frontmatter: PropTypes.shape({
           title: PropTypes.string.isRequired,
           date: PropTypes.string.isRequired,

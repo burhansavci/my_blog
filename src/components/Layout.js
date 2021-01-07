af1a6345614af66ac8675a53bb4a6deee821bf72
Burhan SAVCI <burhansavci@gmail.com>
2021-01-07 17:04:34 +0300

    Update Layout footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -62,7 +62,10 @@ const Layout = ({ data, children, isHome }) => {
         <div className="viewport-bottom">
           <footer className="site-foot">
             <div className="site-foot-nav container">
-              <div className="site-nav-left">
+              <div className="site-foot-nav-left">
+                <Link to="/">{siteMetadata.title}</Link> © 2021
+              </div>
+              <div className="site-foot-nav-right">
                 <Navigation data={siteMetadata.navigation} navClass="site-nav-item" />
               </div>
             </div>

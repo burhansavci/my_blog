8e54af642888c546f1ea0f410714d59f5835ca5e
Burhan SAVCI <burhansavci@gmail.com>
2021-02-08 23:25:07 +0300

    Add i18n config js

---
+++ config/i18n.js
@@ -0,0 +1,21 @@
+module.exports = {
+  en: {
+    default: true,
+    path: `en`,
+    locale: `en-US`,
+    dateFormat: `DD/MM/YYYY`,
+    siteLanguage: `en`,
+    ogLanguage: `en_US`,
+    defaultTitle: `Burhan Savcı Blog`,
+    defaultDescription: `My personal blog site`,
+  },
+  tr: {
+    path: `tr`,
+    locale: `tr-TR`,
+    dateFormat: `DD/MM/YYYY`,
+    siteLanguage: `tr`,
+    ogLanguage: `tr-TR`,
+    defaultTitle: `Burhan Savcı Blog`,
+    defaultDescription: `Benim kişisel blog sitem`,
+  },
+}

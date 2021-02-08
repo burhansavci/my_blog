c47219ef913bf44923f92395b01d568df74a36e4
Burhan SAVCI <burhansavci@gmail.com>
2021-02-08 23:21:00 +0300

    Add removeTrailingSlash and findKey utility functions

--- src/util/utilityFunctions.js
+++ src/util/utilityFunctions.js
@@ -1,12 +1,40 @@
-const slugify = function(text) {
+const slugify = text => {
   return text
-    .toString()
     .toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w-]+/g, '') // Remove all non-word chars
-    .replace(/--+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
+    .replace("ğ", "g") //Replace Turkish characters
+    .replace("ü", "u")
+    .replace("ş", "s")
+    .replace("ı", "i")
+    .replace("ö", "o")
+    .replace("ç", "c")
+    .toString()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+    .replace(/--+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, "") // Trim - from end of text
 }
 
-module.exports = { slugify }
+// Use a little helper function to remove trailing slashes from paths
+const removeTrailingSlash = path =>
+  path === `/` ? path : path.replace(/\/$/, ``)
+
+// From lodash:
+// https://github.com/lodash/lodash/blob/750067f42d3aa5f927604ece2c6df0ff2b2e9d72/findKey.js
+const findKey = (object, predicate) => {
+  let result
+  if (object == null) {
+    return result
+  }
+  Object.keys(object).some(key => {
+    const value = object[key]
+    if (predicate(value, key, object)) {
+      result = key
+      return true
+    }
+    return false
+  })
+  return result
+}
+
+module.exports = { slugify, removeTrailingSlash, findKey }
